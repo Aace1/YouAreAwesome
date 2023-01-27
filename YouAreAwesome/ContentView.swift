9ac2815d33d5be6f4f138444867e0885203c5160
Aaron Anderson <aace2@me.com>
2023-01-27 12:07:00 -0800

    1.11

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imageName = ""
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .padding()
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            
+            Button("Show Message") {
+                let message1 = "You Are Great!"
+                let message2 = "You Are Awesome!"
+                let imageA = "image0"
+                let imageB = "image2"
+                
+                messageString = (messageString == message1 ? message2 : message1)
+                imageName = (imageName == imageA ? imageB : imageA)
+                
+//                if messageString == message1 {
+//                    messageString = message2
+//                    imageName = imageB
+//                } else {
+//                    messageString = message1
+//                    imageName = imageA
+//                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            
+            .padding()
         }
-        .padding()
     }
 }
 
